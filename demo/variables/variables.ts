2a54acab85ba735f78fb540685d4fe096fc4f489
arvee0220 <rowellverdejo@gmail.com>
2024-06-08 12:45:36 -0400

    initial commit

---
+++ demo/variables/variables.ts
@@ -0,0 +1,25 @@
+/* eslint-disable */
+
+// A variable is a named memory location that can hold a value. Variables can
+// be used to store a wide range of data types, such as numbers, strings, and
+// arrays. A variable is declared by specifying its name, data type, and
+// optionally an initial value. Once a variable is declared, it can be read
+// potentially updated in other parts of the program.
+//
+// Useful links:
+// https://www.typescriptlang.org/docs/handbook/variable-declarations.html#let-declarations
+
+const amount = 10;
+const fraction = 10.5;
+const oneThousand = 1e3;
+const allPermissions = 0o777;
+const hexByte = 0xff;
+const binary = 0b0100001;
+const bigInt = 9000n;
+const boolean = true || false;
+
+// completely missing
+const missing = undefined;
+
+// we know it's missing
+const empty = null;
