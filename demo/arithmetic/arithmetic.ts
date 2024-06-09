6000a17c401022c2715c28d66b8a2b97be540dbc
arvee0220 <rowellverdejo@gmail.com>
2024-06-09 08:48:43 -0400

    commit lectures

--- demo/arithmetic/arithmetic.ts
+++ demo/arithmetic/arithmetic.ts
@@ -2,3 +2,38 @@
 import { strict as assert } from "assert";
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+const add = 1 + 1;
+assert.equal(add, 2);
+
+const sub = 1 - 1;
+assert.equal(sub, 0);
+
+const mul = 3 * 3;
+assert.equal(mul, 9);
+
+const div = 10 / 5;
+assert.equal(div, 2);
+
+const inf = 1 / 0;
+assert.equal(inf, Infinity);
+
+const nan = 0 / 0;
+assert(isNaN(nan));
+
+const mod = 10 % 3;
+assert.equal(mod, 1);
+
+const neg = -mod;
+assert.equal(neg, -1);
+
+const neg2 = -neg;
+assert.equal(-neg, 1);
+
+const expo = 2 ** 3;
+assert.equal(expo, 8);
+
+const abs = Math.abs(-5);
+assert.equal(abs, 5);
+
+const pi = Math.PI;
+assert.equal(pi, 3.14)
--- demo/compound-bool/compound-bool.ts
+++ demo/compound-bool/compound-bool.ts
@@ -1,3 +1,25 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+// NOT
+const writing = true;
+const reading = !writing;
+assert.equal(reading, false);
+
+// OR
+const rating = 9;
+const favoriteMovie = false;
+
+const suggestedMovie = rating > 8 || favoriteMovie;
+assert.equal(suggestedMovie, true);
+
+// AND
+const age = 18;
+const isTeenager = age >= 13 && age < 20;
+assert.equal(isTeenager, true);
+
+const packageWeight = 30;
+const packageLength = 50;
+const feeExemption = false;
+
+const extraFee = !feeExemption && (packageWeight > 25 || packageLength > 40);
