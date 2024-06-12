d1c45543ab3c859e81f71cee3628ef20a12c6100
arvee0220 <rowellverdejo@gmail.com>
2024-06-11 20:45:59 -0400

    push lecture notes

--- demo/if-else/if-else.ts
+++ demo/if-else/if-else.ts
@@ -9,3 +9,54 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+
+const answer = 2 + 2;
+assert.equal(answer, 4);
+
+if (answer > 4) {
+	console.log("> 4");
+}
+
+if (answer === 4) {
+	console.log("=4");
+} else if (answer < 4) {
+	console.log("<4");
+} else {
+	console.log("!4");
+}
+
+const hasTheSkills = true;
+const day = "Tuesday";
+const hoursWorked = 9;
+const totalOvertime = 0.5;
+const holidaySeason = false;
+
+const approveWork = () => {
+	if ((hasTheSkills && (isTuesday || (hoursWorked > 8 && totalOvertime < 1))) || holidaySeason) {
+	} else {
+	}
+};
+
+const approveMoreWork = () => {
+	if (!hasTheSkills) {
+		return;
+	}
+
+	if (!hasOvertimeHours(hoursWorked, totalOvertime)) {
+		return;
+	}
+
+	if (!isBusyDay(day, holidaySeason)) {
+	} else {
+		return;
+	}
+};
+
+const isBusyDay = (day: string, holidaySeason: boolean): boolean => {
+	return holidaySeason || day === "Tuesday";
+};
+
+const hasOvertimeHours = (hoursWorked: number, totalOvertime: number): boolean => {
+	const hasHours = hoursWorked > 8 && totalOvertime < 1;
+	return hasHours;
+};
