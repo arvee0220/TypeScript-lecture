8298d697db7c51e704030f7bf7e0c377fcdf6444
arvee0220 <rowellverdejo@gmail.com>
2024-06-12 06:57:31 -0400

    add notes

--- demo/switch/switch.ts
+++ demo/switch/switch.ts
@@ -3,3 +3,29 @@ import { strict as assert } from "assert";
 
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
+
+let someNumber = parseInt("10");
+const sample = "sample";
+const someString = `${sample}`;
+
+switch (someString) {
+	case "test":
+		break;
+	case "sample":
+		break;
+	case "c":
+		break;
+}
+
+if (someString === "test") {
+} else if (someString === "sample") {
+} else if (someString === "c") {
+}
+
+switch (someNumber) {
+	case 1:
+		break;
+	case 2:
+		break;
+	default:
+}
