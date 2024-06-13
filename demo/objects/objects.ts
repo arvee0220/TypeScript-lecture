c7d52ace7b94160d9c80c86a0016e03c35200187
arvee0220 <rowellverdejo@gmail.com>
2024-06-13 16:32:03 -0400

    Push notes

--- demo/objects/objects.ts
+++ demo/objects/objects.ts
@@ -7,3 +7,50 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/objects.html
+
+type Coordinate = {
+	x: number;
+	y: number;
+};
+
+const origin: Coordinate = {
+	x: 0,
+	y: 0,
+};
+
+const { x, y } = origin;
+const xPosition = x;
+const yPosition = y;
+
+assert.equal(xPosition, 0);
+assert.equal(yPosition, 0);
+
+let coord: Coordinate = { x: 1, y: 1 };
+
+coord.x = 20;
+coord.y = 10;
+
+assert.deepEqual(coord, { x: 20, y: 10 });
+
+const printCoord = (coord: Coordinate): undefined => {
+	console.log(coord.x, coord.y);
+};
+
+printCoord(coord);
+printCoord({ x: 5, y: 1 });
+
+type PersonName = string;
+
+type Location = {
+	coord: Coordinate;
+	name: PersonName;
+};
+
+let home: Location = {
+	coord: { x: 0, y: 0 },
+	name: "home",
+};
+
+home.coord.x = 1;
+
+const yPos = home.coord.y;
