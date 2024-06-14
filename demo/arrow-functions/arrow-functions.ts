c30132d854a757e20c5bfb472c5754cb76db1190
arvee0220 <rowellverdejo@gmail.com>
2024-06-14 07:55:52 -0400

    Push lecture notes

--- demo/arrow-functions/arrow-functions.ts
+++ demo/arrow-functions/arrow-functions.ts
@@ -6,6 +6,33 @@
 // to the parent context, making them useful in event handlers and callback
 // functions. They also support implicit return statements for one-liner
 // functions, which makes the code more readable.
-// 
+//
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+const sum = function (lhs: number, rhs: number): number {
+	return lhs + rhs;
+};
+
+const arrowSum = (lhs: number, rhs: number): number => {
+	return lhs + rhs;
+};
+
+const sub = (lhs: number, rhs: number): number => {
+	return lhs - rhs;
+};
+
+const two = sum(1, 1);
+const ten = arrowSum(5, 5);
+
+type CalculateFn = (lhs: number, rhs: number) => number;
+
+function calculate(fn: CalculateFn, lhs: number, rhs: number) {
+	const result = fn(lhs, rhs);
+	console.log(result);
+
+	return result;
+}
+
+calculate(arrowSum, 3, 3);
+calculate(sub, 3, 3);
