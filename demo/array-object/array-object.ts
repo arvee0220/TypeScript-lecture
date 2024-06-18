1d0dd61495637c5d4de683e20216d2594fe6f6c0
arvee0220 <rowellverdejo@gmail.com>
2024-06-18 07:35:22 -0400

    commit lectures

--- demo/array-object/array-object.ts
+++ demo/array-object/array-object.ts
@@ -1,2 +1,23 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
+
+type Link = {
+	title: string;
+	url: string;
+};
+
+const microsoft: Link = {
+	title: "microsoft",
+	url: "microsoft.com",
+};
+
+const typescript: Link = {
+	title: "TypeScript",
+	url: "typescriptlang.org",
+};
+
+const links: Link[] = [microsoft, typescript];
+
+const tsUrl = links[1].url;
+
+links[0].title = "Microsoft";
