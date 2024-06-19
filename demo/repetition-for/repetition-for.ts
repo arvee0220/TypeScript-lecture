11ab771cafb02fde46d4317e7932eaf65e669144
arvee0220 <rowellverdejo@gmail.com>
2024-06-19 07:36:12 -0400

    push lecture notes

--- demo/repetition-for/repetition-for.ts
+++ demo/repetition-for/repetition-for.ts
@@ -16,3 +16,57 @@
 //
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for
+
+//    init    ; bool  ; inc/dec
+for (let i = 1; i <= 5; i++) {
+	console.log(`${i}`);
+}
+
+console.log(`--`);
+
+const nums = [0, 1, 2, 3, 4];
+
+for (let i = 0; i <= nums.length - 1; i++) {
+	console.log(`${i}`);
+}
+
+console.log(`--`);
+
+for (let i = 1; i <= 10000; i++) {
+	console.log(`${i}`);
+	if (i === 3) {
+		break;
+	}
+}
+
+console.log(`--`);
+
+for (let i = 1; i <= 10000; i++) {
+	console.log(`${i}`);
+	if (i === 5) {
+		break;
+	}
+}
+
+console.log(`--`);
+
+for (let i = 1; i <= 10000; i++) {
+	console.log(`${i}`);
+	if (i === 5) {
+		break;
+	}
+
+	if (i === 3) {
+		continue;
+	}
+
+	console.log("hi!");
+}
+
+console.log(`--`);
+
+const letters = ["a", "b", "c"];
+
+for (let i = 0; i < letters.length; i++) {
+	console.log(letters[i]);
+}
--- demo/repetition-while/repetition-while.ts
+++ demo/repetition-while/repetition-while.ts
@@ -2,6 +2,14 @@
 
 // A `while` loop executes the body while (as long as) some boolean expression
 // is `true`. It is your responsibility to manage when and how the loop exits.
-// 
+//
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while
+
+let i = 1;
+
+// Will cause the app to crash since the value of i is not being incremented
+while (i <= 5) {
+	console.log(`${i}`);
+	// Missing increment i += 1
+}
