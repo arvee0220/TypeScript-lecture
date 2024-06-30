49e637aa5b5e34fb09038f539c1138c6212cc9ea
arvee0220 <rowellverdejo@gmail.com>
2024-06-30 17:23:54 -0400

    Push lecture notes

--- demo/exceptions/exceptions.ts
+++ demo/exceptions/exceptions.ts
@@ -9,3 +9,34 @@
 //
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
+
+const divide = (lhs: number, rhs: number): number => {
+	return rhs === 0
+		? (() => {
+				throw new Error("Cannot divide by zero");
+		  })()
+		: lhs / rhs;
+};
+
+const a = divide(10, 5);
+console.log(a);
+
+try {
+	const b = divide(10, 3);
+	console.log(b);
+} catch (e) {
+	console.error(`Error: ${e}`);
+} finally {
+	console.log("divided some numbers");
+}
+
+const div = (lhs: number, rhs: number): number => {
+	try {
+		return divide(lhs, rhs);
+	} catch (error) {
+		console.log("This function is broken");
+		throw error;
+	}
+};
+
+console.log(div(20, 2));
