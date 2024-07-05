39ad76e1316d4028f44bb9347077468e184f7236
arvee0220 <rowellverdejo@gmail.com>
2024-07-05 18:16:08 -0400

    Push lecture notes

--- demo/type-predicates/type-predicates.ts
+++ demo/type-predicates/type-predicates.ts
@@ -12,3 +12,53 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/narrowing.html#using-type-predicates
+
+// Type guard:
+type StrOrNum = string | number;
+
+const sample = (data: StrOrNum) => {
+	if (typeof data === "string") {
+		console.log(`Message: ${data.toUpperCase()}`);
+	} else if (typeof data === "number") {
+		console.log(`Total: ${10 + data}`);
+	}
+};
+
+// Type predicate:
+interface Square {
+	kind: "square";
+	size: number;
+}
+
+interface Circle {
+	kind: "circle";
+	radius: number;
+}
+
+type Shape = Square | Circle;
+
+const isSquare = (shape: Shape): shape is Square => {
+	return shape.kind === "square";
+};
+
+const isCircle = (shape: Shape): shape is Circle => {
+	return shape.kind === "circle";
+};
+
+const calculateArea = (shape: Shape): number => {
+	if (isSquare(shape)) {
+		return shape.size ** 2;
+	}
+
+	if (isCircle(shape)) {
+		return Math.PI * shape.radius ** 2;
+	}
+
+	throw new Error("unknown shape");
+};
+
+const square: Shape = { kind: "square", size: 5 };
+const circle: Shape = { kind: "circle", radius: 2 };
+
+assert.equal(calculateArea(square), 25);
+assert.equal(calculateArea(circle), Math.PI * 2 ** 2);
