9f0a33dae6392be12240bda8455bc79331967d22
arvee0220 <rowellverdejo@gmail.com>
2024-07-11 10:04:53 -0400

    commit notes

--- demo/testing/strings.test.ts
+++ demo/testing/strings.test.ts
@@ -3,3 +3,29 @@ import { concat, div, failedString, slowString } from "./strings";
 it('should say "Hello, world!"', () => {
 	expect(concat("Hello,", " world!")).toEqual("Hello, world!");
 });
+
+it("should divide", () => {
+	expect(div(10, 2)).toEqual(5);
+});
+
+it("should fail to divide by zero", () => {
+	expect(() => {
+		div(10, 0);
+	}).toThrow();
+});
+
+test("slow string fetches sample text", async () => {
+	slowString()
+		.then((data) => {
+			expect(data).toEqual("sample");
+		})
+		.catch((e) => expect(e).toBeUndefined());
+});
+
+test("failed string fails with a 'whoops'", async () => {
+	failedString()
+		.then((data) => {
+			expect(data).toBeUndefined();
+		})
+		.catch((e) => expect(e).toEqual("whoops"));
+});
